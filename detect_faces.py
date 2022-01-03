837fd60e4ff495b75724857d6b5a644f56a5c333
Nachiketa Hebbar <54833985+nachi-hebbar@users.noreply.github.com>
2022-01-03 08:52:50 +0530

    Add files via upload

---
+++ detect_faces.py
@@ -0,0 +1,65 @@
+# USAGE
+# python3 detect_faces.py --image group_selfie.jpg --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
+
+# load the model architecture and weights
+print("[INFO] loading model architecture and weights...")
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+# resize image to 300*300 and normalize it
+print("[INFO] Processing input image...")
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+	(300, 300), (104.0, 177.0, 123.0))
+
+# pass the blob through the network and obtain the detections and
+# predictions
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+
+# loop over the detections
+print("[INFO] Looping over object detections...")
+for i in range(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with the
+	# prediction
+	confidence = detections[0, 0, i, 2]
+
+	# filter detections according to confidence
+	if confidence > args["confidence"]:
+		# compute the (x, y)-coordinates of the bounding box for the face in image
+
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+ 
+		# draw the bounding box of the face along with the associated
+		# probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			(0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
